44b16ed1938229a39ed037f73afe20ae826b4b77
Satho-M <rui92rodrigues@gmail.com>
2023-08-24 16:17:00 +0100

    Add SignalR Hubs Add UserConnection Add ChatHub with Join Room method

--- ChatServices/Program.cs
+++ ChatServices/Program.cs
@@ -1,6 +1,24 @@
+using ChatServices.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddCors(option =>
+{
+    option.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseCors();
+
+app.MapHub<ChatHub>("/chat");
 
 app.Run();

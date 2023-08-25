bfb5a6b68e3179c20e0f97112afab47f826bcc58
Satho-M <rui92rodrigues@gmail.com>
2023-08-25 12:48:05 +0100

    Add UserConnected endpoint on Service Side

--- ChatServices/Hubs/ChatHub.cs
+++ ChatServices/Hubs/ChatHub.cs
@@ -12,12 +12,35 @@ namespace ChatServices.Hubs
             _connections = connections;
         }
 
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                _connections.Remove(Context.ConnectionId);
+                Clients.Group(userConnection.Room)
+                    .SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has left");
+
+                SendConnectedUsers(userConnection.Room);
+            }
+            return base.OnDisconnectedAsync(exception);
+        }
         public async Task JoinRoom(UserConnection userConnection)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
             _connections[Context.ConnectionId] = userConnection;
 
             await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has joined {userConnection.Room}");
+
+            await SendConnectedUsers(userConnection.Room);
+        }
+
+        public Task SendConnectedUsers(string room)
+        {
+            var users = _connections.Values
+                .Where(c => c.Room == room)
+                .Select(c => c.User);
+
+            return Clients.Group(room).SendAsync("UsersInRoom", users);
         }
         public async Task SendMessage(string message)
         {
